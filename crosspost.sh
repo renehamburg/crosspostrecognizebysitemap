16d42caeea632e63c61acb8653b489c2fd1e072c
Rene Sasse <7276222+renehamburg@users.noreply.github.com>
2023-05-03 15:15:06 +0200

    initial Post

---
+++ crosspost.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CACHE_PATH=~/.config/hugo-crosspost-cache
+URL="https://add.here.your.URL/sitemap.xml"
+
+
+if [ ! -d ${CACHE_PATH} ] ; then
+ echo "mkdir ${CACHE_PATH}"
+ mkdir ${CACHE_PATH}
+fi
+
+mv ${CACHE_PATH}/current ${CACHE_PATH}/latest
+wget -q -O ${CACHE_PATH}/current ${URL}
+
+DIFF=$(diff ${CACHE_PATH}/current ${CACHE_PATH}/latest)
+
+if ! diff ${CACHE_PATH}/current ${CACHE_PATH}/latest > /dev/null 2>&1
+then
+
+ POST=$(diff ${CACHE_PATH}/current ${CACHE_PATH}/latest | grep "<loc>" | sed s/\>/\ /g | sed s/\</\ /g|awk '{print $2}')
+ TITEL=$(wget -qO- ${POST}| perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+ 
+ echo "Sending toot: 'hey; neuer Artikel ${TITEL} im Blog! #blogpost $POST'"
+ toot 'hey; neuer Artikel ${TITEL} im Blog! #blogpost $POST'
+fi
